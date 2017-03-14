60b859a82e26239ad7e21dc4b959de9f217da508
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-03-14 23:53:38 +0100

    Added Posts Model & View

--- src/posts/models.py
+++ src/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=50)
+    intro = models.TextField(blank=True, null=True)
+    body = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)  # Automáticamente añada la fecha de creación
+    modified_at = models.DateTimeField(auto_now=True)  # Automáticamente actualiza la fecha al guardar
+    # Category FK
+    # User FK
--- src/posts/views.py
+++ src/posts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from posts.models import Post
+
+
+def posts_list(request):
+    """
+    Recupera todas los posts de la BBDD y las pinta
+    :param request: objeto HttpRequest
+    :return
+    """
+
+    # Recuperar todas los posts de la BBDD
+    posts = Post.objects.all()
+
+    # Comprobamos si se debe filtrar por posts creados por el user autenticado
+
+
+    # Comprobamos si se debe filtrar por posts creadas por el user autenticado
+
+
+    # Devolver la respuesta
+    context = {
+        'task_objects': posts
+    }
+    return render(request, 'posts/list.html', context)
